0888dface183911c7f65d7ed3804416a7be5acfd
Dex <sheepareawesome777@gmail.com>
2022-08-28 00:41:45 -0400

    Fixed lifetime error in open_dialog()

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use std::thread::sleep;
 
 #[tokio::main]
 async fn main() {
+	let mut iteration = 1;
+
 	loop {
-		tokio::spawn(open_dialog());
+		let title = format!("Process #{}", iteration);
+		tokio::spawn(open_dialog(title, "spoon :D"));
 		wait(2000);
+		iteration += 1;
 	}
 }
 
@@ -14,11 +18,14 @@ fn wait(time: u64) -> () {
 	sleep(Duration::from_millis(time));
 }
 
-async fn open_dialog() {
+async fn open_dialog<Tt: AsRef<str>, Tx: AsRef<str>>(title: Tt, text: Tx) {
+	let rtitle = title.as_ref();
+	let rtext = text.as_ref();
+
 	MessageDialog::new()
 	.set_type(MessageType::Warning)
-	.set_title("Imagine getting hacked!")
-	.set_text("Clown!")
+	.set_title(&rtitle)
+	.set_text(&rtext)
 	.show_confirm()
 	.unwrap();
 }
