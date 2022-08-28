0e471331c4c84ab0af34d90e717cb6008a0c0f23
Dex <sheepareawesome777@gmail.com>
2022-08-27 23:11:39 -0400

    Async Await...???? (still learning Rust)

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bogus Amogus Virus
+
+I'm making this little program as a "safe malware" app to run in one of my YouTube videos,
+so I can show "suspicious" files, without the risk of getting a real crypto miner or any
+other garbage like that onto my real host system.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use native_dialog::{MessageDialog, MessageType};
+use futures::executor::block_on;
+use std::time::Duration;
+use std::thread::sleep;
 
 fn main() {
-	let yes = MessageDialog::new()
-		.set_type(MessageType::Info)
-		.set_title("Imagine getting hacked!")
-		.set_text("Clown!")
-		.show_confirm()
-		.unwrap();
+	block_on(main_async());
+}
+
+async fn main_async() {
+	loop {
+		open_dialog().await;
+		wait(2000).await;
+		//futures::join!(open_dialog(), wait(2000));
+	}
+}
+
+async fn wait(time: u64) -> () {
+	sleep(Duration::from_millis(time));
+}
+
+async fn open_dialog() {
+	MessageDialog::new()
+	.set_type(MessageType::Warning)
+	.set_title("Imagine getting hacked!")
+	.set_text("Clown!")
+	.show_confirm()
+	.unwrap();
 }
